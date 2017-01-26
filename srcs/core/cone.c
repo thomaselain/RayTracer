abe15984e8f4e3a08bd5309b7c2b2af1907f073a
Sebastien VASSAL <svassal@e1r2p19.42.fr>
2017-01-26 22:27:41 +0100

    Minor Fix

--- srcs/core/cone.c
+++ srcs/core/cone.c
@@ -6,7 +6,7 @@
 /*   By: svassal <svassal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/26 19:32:35 by svassal           #+#    #+#             */
-/*   Updated: 2017/01/26 20:02:39 by svassal          ###   ########.fr       */
+/*   Updated: 2017/01/26 22:26:59 by svassal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,15 @@ float		find_cone_inter(t_ray *r, t_object *o)
 {
 	float		f[6];
 
-	A = vector_dot(r->direction, e->direction) - powf(vector_dot(r->direction,
+	A = vector_dot(r->direction, r->direction) - powf(vector_dot(r->direction,
 		o->direction), 2) * (1 + powf(tanf(o->angle / 2), 2));
-	B = 2 * ((vector_dot(r->direction, vector_sub(o->origin, o->origin))) - (1 +
-		powf(tanf(o->angle / 2), 2)) * vector_dot(r->direction, o->direction) *
-			vector_dot(vector_sub(r->origin, o->origin), o->direction));
-	C = vector_dot(vector_sub(r->origin, o->origin), vector_sub(r->origin,
+	B = 2 * ((vector_dot(r->direction, vector_sub_vec(r->origin, o->origin))) -
+		(1 + powf(tanf(o->angle / 2), 2)) * vector_dot(r->direction,
+			o->direction) * vector_dot(vector_sub_vec(r->origin, o->origin),
+				o->direction));
+	C = vector_dot(vector_sub_vec(r->origin, o->origin), vector_sub_vec(r->origin,
 		o->origin)) - (1 + powf(tanf(o->angle / 2), 2)) * powf(vector_dot(
-			vector_sub(r->originm, o->origin), o->direction));
+			vector_sub_vec(r->origin, o->origin), o->direction), 2);
 	D = powf(B, 2) - 4 * A * C;
 	if (o)
 		;
