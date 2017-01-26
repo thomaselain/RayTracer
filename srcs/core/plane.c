a70d9ae44f5408e4fac3dac7e623c00667221605
Sebastien VASSAL <svassal@e1r2p19.42.fr>
2017-01-26 18:56:15 +0100

    Push

--- srcs/core/plane.c
+++ srcs/core/plane.c
@@ -6,9 +6,13 @@
 /*   By: svassal <svassal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/26 18:16:21 by svassal           #+#    #+#             */
-/*   Updated: 2017/01/26 18:16:31 by svassal          ###   ########.fr       */
+/*   Updated: 2017/01/26 18:56:02 by svassal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <core.h>
 
+float		find_plane_inter(t_scene *s, t_ray *r, t_object *o)
+{
+
+}
