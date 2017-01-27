833d35e845774eb5233e86c869874dad9f01a8de
Sebastien VASSAL <svassal@e1r5p17.42.fr>
2017-01-27 22:30:39 +0100

    Fixed functions

--- srcs/core/cone.c
+++ srcs/core/cone.c
@@ -6,11 +6,12 @@
 /*   By: svassal <svassal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/26 19:32:35 by svassal           #+#    #+#             */
-/*   Updated: 2017/01/27 16:43:34 by svassal          ###   ########.fr       */
+/*   Updated: 2017/01/27 22:28:03 by svassal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <core.h>
+#include <vector.h>
 #define A			f[0]
 #define B			f[1]
 #define C			f[2]
@@ -26,19 +27,19 @@ float		find_cone_inter(t_ray *r, t_object *o)
 {
 	float		f[6];
 
-	A = vector_dot(r->direction, r->direction) - powf(vector_dot(r->direction,
+	A = vector_dot(r->dir, r->dir) - powf(vector_dot(r->dir,
 		o->direction), 2) * (1 + powf(tanf(o->angle / 2), 2));
-	B = 2 * ((vector_dot(r->direction, vector_sub_vec(r->origin, o->origin))) -
-		(1 + powf(tanf(o->angle / 2), 2)) * vector_dot(r->direction,
-			o->direction) * vector_dot(vector_sub_vec(r->origin, o->origin),
+	B = 2 * ((vector_dot(r->dir, vector_sub_vec(r->pos, o->origin))) -
+		(1 + powf(tanf(o->angle / 2), 2)) * vector_dot(r->dir,
+			o->direction) * vector_dot(vector_sub_vec(r->pos, o->origin),
 				o->direction));
-	C = vector_dot(vector_sub_vec(r->origin, o->origin), vector_sub_vec(
-		r->origin, o->origin)) - (1 + powf(tanf(o->angle / 2), 2)) *
-			powf(vector_dot(vector_sub_vec(r->origin, o->origin),
+	C = vector_dot(vector_sub_vec(r->pos, o->origin), vector_sub_vec(
+		r->pos, o->origin)) - (1 + powf(tanf(o->angle / 2), 2)) *
+			powf(vector_dot(vector_sub_vec(r->pos, o->origin),
 				o->direction), 2);
 	D = powf(B, 2) - 4 * A * C;
 	if (D < 0.0)
-		return (MAX_SIZE);
+		return (10000);
 	RES_0 = (-B - sqrtf(D)) / (A + A);
 	RES_1 = (-B + sqrtf(D)) / (A + A);
 	return ((RES_0 < RES_1) ? (RES_0) : (RES_1));
--- srcs/core/plane.c
+++ srcs/core/plane.c
@@ -6,7 +6,7 @@
 /*   By: svassal <svassal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/26 18:16:21 by svassal           #+#    #+#             */
-/*   Updated: 2017/01/27 15:40:22 by svassal          ###   ########.fr       */
+/*   Updated: 2017/01/27 22:29:54 by svassal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 
 float		find_plane_inter(t_ray *r, t_object *o)
 {
-	return (-((o->direction.x * (r->origin.x - o->origin.x) + o->direciton.y *
-		(r->origin.y - o->origin.y) + o->direction.z * (r->origin.z -
-			o->origin.z)) / (o->direction.x * r->direction.x + o->direction.y *
-				r->direction.y + o->direction.z * r->direction.z)));
+	return (-((o->direction.x * (r->pos.x - o->origin.x) + o->direction.y *
+		(r->pos.y - o->origin.y) + o->direction.z * (r->pos.z -
+			o->origin.z)) / (o->direction.x * r->dir.x + o->direction.y *
+				r->dir.y + o->direction.z * r->dir.z)));
 }
