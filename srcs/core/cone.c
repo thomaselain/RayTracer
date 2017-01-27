bb3fe8db9182fd4d740a2f71d415ad9d432840d1
Sebastien VASSAL <svassal@e1r5p17.42.fr>
2017-01-27 16:50:08 +0100

    Fixed norme

--- srcs/core/cone.c
+++ srcs/core/cone.c
@@ -6,7 +6,7 @@
 /*   By: svassal <svassal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/26 19:32:35 by svassal           #+#    #+#             */
-/*   Updated: 2017/01/27 15:38:20 by svassal          ###   ########.fr       */
+/*   Updated: 2017/01/27 16:43:34 by svassal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,10 @@ float		find_cone_inter(t_ray *r, t_object *o)
 		(1 + powf(tanf(o->angle / 2), 2)) * vector_dot(r->direction,
 			o->direction) * vector_dot(vector_sub_vec(r->origin, o->origin),
 				o->direction));
-	C = vector_dot(vector_sub_vec(r->origin, o->origin), vector_sub_vec(r->origin,
-		o->origin)) - (1 + powf(tanf(o->angle / 2), 2)) * powf(vector_dot(
-			vector_sub_vec(r->origin, o->origin), o->direction), 2);
+	C = vector_dot(vector_sub_vec(r->origin, o->origin), vector_sub_vec(
+		r->origin, o->origin)) - (1 + powf(tanf(o->angle / 2), 2)) *
+			powf(vector_dot(vector_sub_vec(r->origin, o->origin),
+				o->direction), 2);
 	D = powf(B, 2) - 4 * A * C;
 	if (D < 0.0)
 		return (MAX_SIZE);
