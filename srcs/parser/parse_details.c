532c0962ee952d5e1219001c539ffc29aa34e193
cchicote <comechicoteau@gmail.com>
2017-02-06 19:02:54 +0100

    SQUARE

--- srcs/parser/parse_details.c
+++ srcs/parser/parse_details.c
@@ -142,8 +142,8 @@ t_type		parse_type(char **string)
 		ret = DIRECTIONAL;
 	else if (ft_strnstr(*string, "\"CIRCLE\"", (index = 8)) != 0)
 		ret = CIRCLE;
-	// else if (ft_strnstr(*string, "\"SQUARE\"", (index = 8)) != 0)
-	// 	ret = SQUARE;
+	else if (ft_strnstr(*string, "\"SQUARE\"", (index = 8)) != 0)
+		ret = SQUARE;
 	else
 		index = 0;
 	*string = *string + index;
