c32b512c63869c069e0f1047836c73299a2fb839
cchicote <comechicoteau@gmail.com>
2017-02-06 18:24:12 +0100

    circles

--- srcs/parser/parse_objects.c
+++ srcs/parser/parse_objects.c
@@ -49,10 +49,7 @@ t_object		parse_object(char **string)
 			fill_objects(&tmp, &ret, 0);
 			ERR = (valide_coma(tmp) == ERROR) ? (ERROR) : (ERR);
 			if (ERR == ERROR)
-			{
-				ft_putendl(tmp);
 				error_close(OBJECT, CEC);
-			}
 			*string = tmp;
 			INDEX = -1;
 		}
