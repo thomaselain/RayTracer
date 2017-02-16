5814f62159fbe34b8d20da260a86b34851041a3e
Thomas ELAIN <telain@e2r4p9.42.fr>
2017-02-16 17:33:05 +0100

    Bruit de perlin.

--- srcs/core/color_modifications.c
+++ srcs/core/color_modifications.c
@@ -6,7 +6,7 @@
 /*   By: telain <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/26 16:49:13 by telain            #+#    #+#             */
-/*   Updated: 2017/02/12 19:22:39 by telain           ###   ########.fr       */
+/*   Updated: 2017/02/16 17:32:21 by telain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 #include <img.h>
 #include <color.h>
 
+float		vector_dist(t_vector4f v1, t_vector4f v2);
+
 int			adjust_color(t_scene *s, t_object *hit, t_ray ray)
 {
 	t_object	*first_hit;
 	t_list		*light;
 	t_ray		v_light;
-	t_ray		prev_ray;
 	float		coef;
 	int			c;
 	int			i;
@@ -33,35 +34,30 @@ int			adjust_color(t_scene *s, t_object *hit, t_ray ray)
 	light = s->lights;
 	while (hit != 0 && hit->reflection > 0.0 && ++i <= MAX_REFLECTION)
 	{
-		prev_ray = ray;
 		first_hit = hit;
 		hit = get_reflect(s, hit, &ray);
 	}
 	if (hit != 0)
 	{
 		c = hit->color;
-		if (hit->type == PLANE)
-			c = find_grid_color(hit, ray); 
 		while (light != 0)
 		{
-			coef *= find_shadow(s, hit, ray, v_light);
-//			c = color_add(hit->color, color_mul(((t_object*)light->content)->color, specular_light(s, hit, ray, v_light.pos)));           //Work in progress :/
-			if (first_hit)
-				c = color_mul(c, first_hit->reflection);
-			c = color_div(c, vector_dist(ray.pos, hit->origin) / 100 + 1.0); // effet brouillard lointain
-			c = first_hit != 0 ? color_add(c, color_mul(color_avg(c, first_hit->color), 1)) : c;
-			c = color_mul(c, noise(hit, ray.pos));
-
-			v_light.pos = ((t_object*)light->content)->origin;
 			v_light.dir = get_light_vector((t_object*)light->content, ray);
 			if (vector_dot(v_light.dir, get_normal(hit, ray)) >= 0)
 				c = color_mul(c, vector_dot(v_light.dir, get_normal(hit, ray)));
 			else
 				c = 0;
+			coef *= find_shadow(s, hit, ray, v_light);
+			coef *= noise(hit, ray.pos);
+			if (hit->type == PLANE)
+				c = find_grid_color(hit, ray); 
+//			c = color_add(hit->color, color_mul(((t_object*)light->content)->color, specular_light(s, hit, ray, v_light)));      //     Work in progress :/
+				c = color_div(c, vector_dist(ray.pos, hit->origin) / 10 + 1.0);  //effet brouillard lointain
+				c = color_mul(c, coef);
 			light = light->next;
-		}	
-		c = color_mul(c, coef);
+		}
 	}
+	c = first_hit != 0 ? color_add(c, color_mul(color_avg(c, first_hit->color), 1)) : c;
 	return (c);
 }
 
@@ -73,3 +69,4 @@ t_object	*get_reflect(t_scene *s, t_object *hit, t_ray *ray)
 	ray->dir = ADD(ray->dir, MUL(normal, -2.0 * vector_dot(normal, ray->dir)));
 	return (get_intersection(s, ray));
 }
+
