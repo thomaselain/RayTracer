302eb94d4fcfc924f0564126214a12c5f20ffdd0
Thomas ELAIN <telain@e2r4p9.42.fr>
2017-02-16 19:47:07 +0100

    Preparation au merge

--- srcs/core/color_modifications_recursive.c
+++ srcs/core/color_modifications_recursive.c
@@ -6,7 +6,7 @@
 /*   By: telain <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/16 18:42:04 by telain            #+#    #+#             */
-/*   Updated: 2017/02/16 18:48:58 by telain           ###   ########.fr       */
+/*   Updated: 2017/02/16 19:46:38 by telain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int		adjust_color(t_scene *s, t_object *hit, t_ray ray, int reflects)
 {
 	if (hit != 0 && hit->reflection > 0.0 && reflects <= MAX_REFLECTION)
 	{
+
 		return (color_mul(hit->reflection, adjust_color(s, get_reflect(s, hit, &ray), ray, reflects + 1));
 	}
 }
