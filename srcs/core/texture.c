023d6a8540a2d0a33459eaf7df541b5e0fc49a56
Come CHICOTEAU <cchicote@student.42.fr>
2017-04-03 20:37:59 +0200

    few corrections for the norminette

--- srcs/core/texture.c
+++ srcs/core/texture.c
@@ -14,7 +14,7 @@
 #include <ray.h>
 #include <color.h>
 #include <img.h>
-#define	NORMAL(X) vector_normalize(X)
+#define NORMAL(X) vector_normalize(X)
 #define ROTATE(X, Y, Z) vector_rotate(X, Y, Z)
 
 unsigned int		get_texture_pixel(t_object *hit, t_ray ray)
@@ -60,13 +60,14 @@ unsigned int		find_plane_texture(t_object *o, t_ray ray)
 	t = vector_normalize(t);
 	d = vector_dist(ray.pos, o->origin);
 	alpha = acos(vector_dot(t, vector_rotate(vector_normalize(SUB(o->origin,
-							ray.pos)), o->direction, 0 /*remplacer scale par rotate*/)));
+		ray.pos)), o->direction, 0)));
+	/*remplacer scale par rotate*/
 	u = cos(alpha) * d / o->texture.scale;
 	if (ray.pos.z < 0)
 		v = sin(-alpha) * d / o->texture.scale;
 	else
 		v = sin(alpha) * d / o->texture.scale;
-	// v = v < 0 ? v + 1 : v;
+	/* v = v < 0 ? v + 1 : v;*/
 	v += MAX_SIZE;
 	return (img_get_pixel(&o->texture,
 				(int)(u * o->texture.w + 1) % o->texture.w,
