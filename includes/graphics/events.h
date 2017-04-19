3046f80b49e5cc17da76fb35bdae815703efd537
aljourda <aljourda@lex.fr>
2017-04-19 18:00:59 +0200

    header norm

--- includes/graphics/events.h
+++ includes/graphics/events.h
@@ -6,7 +6,7 @@
 /*   By: aljourda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 17:40:20 by aljourda          #+#    #+#             */
-/*   Updated: 2017/04/19 17:41:50 by aljourda         ###   ########.fr       */
+/*   Updated: 2017/04/19 18:00:40 by aljourda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include <SDL2/SDL.h>
 # include <core.h>
 
-int	        key_status_loop(t_scene *e);
-int		    key_status_progress(void);
+int		key_status_loop(t_scene *e);
+int		key_status_progress(void);
 
 #endif
